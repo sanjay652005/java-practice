e8b95657dfa64f1a9889440871bc820e1fc1d5d8
Sanjay <sanjay6379926402@gmail.com>
2026-03-27 21:12:28 +0530

    Add static block, instance block, and constructor execution example

---
+++ src/exercise/statics.java
@@ -0,0 +1,23 @@
+package exercise;
+
+public class statics {
+	statics(){
+		System.out.println("statics contract called");
+	}
+	static {System.out.println("static block 1");}
+	static {System.out.println("statiic block 2");}
+	{
+		System.out.println("object block 1");
+	}
+	{
+		System.out.println("object block 2");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		statics m=new statics();
+		System.out.println("called");
+		statics m1=new statics();
+	}
+}
